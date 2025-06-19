a1a37327a1498d1410af724ca0d8d282be6e8472
Alfie134 <alberteoegendahl@gmail.com>
2025-06-19 10:35:58 +0200

    wE UP AND RUNNING bthces

---
+++ src/app/components/creativity/creativity.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creativity',
+  standalone: true,
+  templateUrl: './creativity.html',
+  styleUrl: './creativity.css'
+})
+export class CreativityComponent {
+
+}
