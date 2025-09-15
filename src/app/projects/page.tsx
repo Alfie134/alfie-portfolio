aea1bf78bbe12ef6eceb921553ae077594c11874
Alfie134 <alberteoegendahl@gmail.com>
2025-09-15 19:15:09 +0200

    maybehappy

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,11 @@
+export default function ProjectsPage() {
+    return (
+        <section className="min-h-screen flex flex-col justify-center items-center 
+        bg-gradient-to-b from-[#f4d3be] to-[#feddf1] text-center pt-20">
+            <h1 className="text-4xl font-bold mb-4">The Project</h1>
+            <p className="text-lg max-w-xl">
+                Here you can see what is currently going on with our semester project &apos;HorseEvent&apos;.
+            </p>
+        </section>
+    );
+}
