7c1b6ad1181786bd507b8a4a04b0c94d81d34327
Alfie134 <alberteoegendahl@gmail.com>
2025-09-15 18:51:05 +0200

    idk\

--- portfolio/src/app/components/Header.tsx
+++ portfolio/src/app/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="bg-gradient-to-r from-purple-300 via-pink-200 to-blue-200 shadow-md fixed w-full z-50">
             <nav className="max-w-5xl mx-auto px-4 py-4">
-                <ul className="flex justify-center space-x-8 font-semibold -text-gray-800">
+                <ul className="flex justify-center space-x-8 font-semibold -text-brown-800">
                     <li><Link href="/" className="hover:text-purple-600 transiton-colors duration-300"> Home </Link></li>
                     <li><Link href="/about" className="hover:text-purple-600 transiton-colors duration-300">About</Link></li>
                     <li><Link href="/projects" className="hover:text-purple-600 transiton-colors duration-300">Projects</Link></li>
@@ -12,5 +12,5 @@ export default function Header() {
                 </ul>
             </nav>
         </header>
-    );    
+    );
 }
