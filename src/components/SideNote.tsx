244c57aca7992c2e052bbca4477dfdfad89555b3
Alfie134 <alberteoegendahl@gmail.com>
2026-04-07 12:01:40 +0200

    arched portrait commented out for test reasons

--- src/components/SideNote.tsx
+++ src/components/SideNote.tsx
@@ -1,15 +1,16 @@
-type ArchedPortritProps = {
-    src?: string;
-    alt?: string;
-    className?: string; //sizing wrapper
-    bgToneClassName?: string; //Colour behind photo inside arch
-    focus?: string; //SVG preserveAspectRatio alignment
-};
+// type ArchedPortritProps = {
+//     src?: string;
+//     alt?: string;
+//     className?: string; //sizing wrapper
+//     bgToneClassName?: string; //Colour behind photo inside arch
+//     focus?: string; //SVG preserveAspectRatio alignment
+// };
 
-export default function ArchedPortrait({
-
-}; ArchedPortraitProps) {
-    return (
-        <div></div>
-    )
-}
+// export default function ArchedPortrait({
+    
+// }; 
+// ArchedPortraitProps) {
+//     return (
+//         <div></div>
+//     )
+// }
